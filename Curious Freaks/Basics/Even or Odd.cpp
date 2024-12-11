b124163c2015994053f921c8dbb33ef4a1f8c136
Madhumidha-S <madhumidha072005@gmail.com>
2024-12-11 23:46:05 +0530

    Basics

---
+++ Curious Freaks/Basics/Even or Odd.cpp
@@ -0,0 +1,15 @@
+/*Even or Odd*/
+
+class Solution {
+  public:
+    string oddEven(int n) {
+        if(n%2 == 0)
+        {
+            return "even";;
+        }
+        else
+        {
+            return "odd";
+        }
+    }
+};
