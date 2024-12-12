7410fad8a7ae3caa5b23663b4be6de27912f5120
Madhumidha-S <madhumidha072005@gmail.com>
2024-12-12 19:05:33 +0530

    Basics

---
+++ Curious Freaks/Basics/Prime Number.cpp
@@ -0,0 +1,19 @@
+/*Prime Number*/
+
+class Solution {
+  public:
+    bool isPrime(int n) {
+        if(n==1)
+        {
+            return false;
+        }
+        for(int i = 2; i<n/2; i++)
+        {
+            if(n%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
